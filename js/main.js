0a0d98f7ca6b5e469c1d7a0e391dc1987c87d1b0
AlexisFioreCode <alexis.fiorentino.pro@gmail.com>
2021-04-15 10:38:56 +0200

    button to play the game

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 //Variable des couleurs
 let colors = ["red","red","green","green","blue","blue","yellow","yellow","orange","orange","purple","purple"];
 //contenant les couleurs des cartes retournées à un moment donné du jeu.
-let cardColors=[];
+let cardColor=[];
 //Cette variable contient le nombre de paires de cartes qui ont déjà été trouvées.
 let score=0;
 //variable avec mes divs
@@ -10,18 +10,48 @@ console.log(allcards);
 for(let i =0 ; i< allcards.length ; i++) {
     allcards[i].onclick = function(){
         allcards[i].style.background= colors[i];
-        cardColors.push(allcards[i])
-        console.log(cardColors);
-    };
-    
+        cardColor.push(allcards[i])
+        console.log(cardColor); 
+        if (window.getComputedStyle(cardColor[0],null).getPropertyValue("background") === window.getComputedStyle(cardColor[1],null).getPropertyValue("background")); {
+            alert("Bien joué 1 points");  
+            cardColor = []; 
+        }        
+    };    
 };
 
 
-    /*  if (window.getComputedStyle(cardColor[0],null).getPropertyValue("background") === window.getComputedStyle(cardColor[1],null).getPropertyValue("background")); {
-        
-    }   */
-   /* let addColors = colors[Math.floor(Math.random() * colors.length)];
-    cardColors.push(addColors);
-    console.log(); */
+
+    /*      else {
+        cardColor[0].style.background = "#000000";
+        cardColor[1].style.background = "#000000";
+    } */
+  
+   //Fonctions pour faire fonctionner le bouton 
+let game =document.getElementById("thegame")
+let btntext = document.getElementById("play")
+function getResolution() { 
+    //si la résolution de l'écran est de moins de 768px  
+    if(screen.width < 768) {
+        btntext.innerText = "Veuillez vous mettre en mode paysage pour jouer"
+    } else {
+        btntext.innerText = "Jouer au jeu"
+    }
+}
+function removeBtn(){
+    btntext.remove()
+    game.classList.remove("d-none");
+    game.classList.add("d-block");
+}
+
+
+    
+   /* const shuffleArray = array => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+}*/
 
 
